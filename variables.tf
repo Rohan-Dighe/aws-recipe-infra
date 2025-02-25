b6bc8d43821244ed3dc5e410877daaf40f40fc01
Rohan Dighe <134312516+Rohan-Dighe@users.noreply.github.com>
2025-02-25 18:32:58 +0200

    Create variables.tf
    Updated variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "s3_recipe_bucket" {
+  default = "automation-recipe-storage"
+}
+
+variable "s3_archive_bucket" {
+  default = "automation-recipe-archives"
+}
